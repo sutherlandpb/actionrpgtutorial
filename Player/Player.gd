961a16c0f399495b57545ee3da405d8a9ee88aaa
andrea sutherland <sutherlandpb@gmail.com>
2021-12-29 10:43:44 -0500

    added attack animations and buttons

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,14 +4,33 @@ const ACCELERATION = 500
 const MAX_SPEED = 80 
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = $AnimationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+	
 # Called every physics tick
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -20,9 +39,19 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/run/blend_position", input_vector)
+		animationTree.set("parameters/attack/blend_position", input_vector)
 		animationState.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:		
 		animationState.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)	
 	velocity = move_and_slide(velocity)
+	if Input.is_action_just_pressed("Attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("attack")
+	
+func attack_animation_finish():
+	state = MOVE

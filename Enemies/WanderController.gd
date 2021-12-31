d6b8ccb78278ffec65b62a8231d486c180b0366b
andrea sutherland <sutherlandpb@gmail.com>
2021-12-30 19:37:38 -0500

    bats will now wander a little

---
+++ Enemies/WanderController.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export var wander_range = 32
+
+onready var startPosition = global_position
+onready var targetPosition = global_position
+onready var timer = $Timer
+
+func update_target_position():
+	var target_vector = Vector2(rand_range(-wander_range, wander_range), rand_range(-wander_range, wander_range))
+	targetPosition = startPosition + target_vector
+
+func _on_Timer_timeout():
+	update_target_position()
+
+func get_time_left():
+	return timer.time_left
+
+func start_wander_timer(duration):
+	timer.start(duration)
+	
